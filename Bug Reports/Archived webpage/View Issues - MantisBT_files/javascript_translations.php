6f8aaaebc2acf244907305c1b2edae03dcc0e62d
SunlistOG <sunlist.og@outlook.com>
2021-10-13 18:28:12 +0300

    Add the solutions: screenshots and other files

---
+++ Bug Reports/Archived webpage/View Issues - MantisBT_files/javascript_translations.php
@@ -0,0 +1,7 @@
+var translations = new Array();
+translations['time_tracking_stopwatch_start'] = 'Start';
+translations['time_tracking_stopwatch_stop'] = 'Stop';
+translations['loading'] = 'Loading...';
+translations['form_error_required'] = 'This field is required';
+translations['form_error_valid_email'] = 'Please enter the valid email';
+translations['form_error_maxlength'] = 'Please enter no more than {0} characters.';
